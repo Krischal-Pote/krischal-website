0647bdd74a4194cb8dede0f276788dd496c5a5ba
Krischal Om Pote <crischal1234@gmail.com>
2024-09-12 16:03:48 +0545

    remove:vite port

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,5 @@ import { defineConfig } from "vite";
 import vercel from "vite-plugin-vercel";
 
 export default defineConfig({
-  server: {
-    port: process.env.PORT,
-  },
   plugins: [vercel()],
 });
