6fe799f585ba88a517351255a844d34f5a5e2ecf
Krischal Om Pote <crischal1234@gmail.com>
2024-09-12 15:46:41 +0545

    add:versel deploy code

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,10 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import vercel from "@sveltejs/adapter-vercel";
 
-export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-}
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+    adapter: vercel(),
+  },
+};
+
+export default config;
